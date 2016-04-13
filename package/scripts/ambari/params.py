63423a28fc879a5bdb0d012bf9c98831f8c52107
radcheb <radcheb@gmail.com>
2016-04-13 17:54:37 +0200

    remove custom lib files

--- package/scripts/ambari/params.py
+++ package/scripts/ambari/params.py
@@ -10,14 +10,12 @@ r_libs = filter(lambda x: len(x.strip()) > 0, config['configurations']['r-config
 r_repo_url = config['configurations']['r-config']['r.repo_url']
 
 package_dir = helpers.package_dir()
-files_dir = package_dir + 'files/'
 scripts_dir = package_dir + 'scripts/'
 
 r_libs_dir = '/usr/lib64/R/library/'
 
 commands = []
-commands.append('sh ' + scripts_dir + 'shell/r_lib_setup.sh ' + files_dir + ' ' + scripts_dir + ' ' + r_libs_dir)
 
 for lib in r_libs:
-  command = ' '.join(['sh', scripts_dir+'shell/r_lib_install.sh', files_dir, lib, r_libs_dir, r_repo_url])
+  command = ' '.join(['sh', scripts_dir+'shell/r_lib_install.sh', lib, r_libs_dir, r_repo_url])
   commands.append(command)
--- package/scripts/shell/r_lib_install.sh
+++ package/scripts/shell/r_lib_install.sh
@@ -1,16 +1,10 @@
 set -eu
 
-FILES_DIR=$1
-R_LIB=$2
-R_LIBS_DIR=$3
-REPO_URL=$4
+R_LIB=$1
+R_LIBS_DIR=$2
+REPO_URL=$3
 
 if [ ! -d "$R_LIBS_DIR/$R_LIB" ]
 then
-  if [ -f "$FILES_DIR/libs/$R_LIB.tgz" ]
-  then
-    tar -xzvf $FILES_DIR/libs/$R_LIB.tgz -C $R_LIBS_DIR
-  else
-    Rscript -e 'install.packages(c("'$R_LIB'"), repos="'$REPO_URL'");'
-  fi
+  Rscript -e 'install.packages(c("'$R_LIB'"), repos="'$REPO_URL'");'
 fi
